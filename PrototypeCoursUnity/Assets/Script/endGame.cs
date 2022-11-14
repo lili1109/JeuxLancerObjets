11eaea189c44df822f09b59c19efa76d21b41398
lisa hersant <lisa.hers11@gmail.com>
2022-11-14 19:37:42 +0100

    UpdateUI et Detect robor

--- PrototypeCoursUnity/Assets/Script/endGame.cs
+++ PrototypeCoursUnity/Assets/Script/endGame.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class endGame : MonoBehaviour
 {
+    public GameObject endGameMenu;
+    private void Start()
+    {
+        endGameMenu.SetActive(false);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Player")
         {
+            endGameMenu.SetActive(true);
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
